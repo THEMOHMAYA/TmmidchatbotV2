fb7f4513a3fdd14ee2ee69616bd2433d4d2c688e
MR MOH MAYA <124359102+THEMOHMAYA@users.noreply.github.com>
2024-04-02 14:03:37 +0530

    Update __main__.py

--- Venom/__main__.py
+++ Venom/__main__.py
@@ -23,4 +23,4 @@ async def anony_boot():
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(anony_boot())
-    LOGGER.info("Stopping Venom Bot...")
+    LOGGER.info("Stopping the Bot...")
